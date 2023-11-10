dba02343f2213a175cca7d24f635c364b09e6aac
Lizz <lizazinha2.0@gmail.com>
2023-11-10 16:08:54 -0300

    versão 3

---
+++ Fundamentos/FormatandoNumero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CursoUdemy.Fundamentos {
+    public class FormatandoNumero {
+        public static void Executar() {
+            double valor = 15.175;
+            Console.WriteLine(valor.ToString("F1"));
+            Console.WriteLine(valor.ToString("C")); //moeda real
+            Console.WriteLine(valor.ToString("P")); //porcentagem
+            Console.WriteLine(valor.ToString("#.##"));
+
+            //configurar a lingua de padronização,
+            //nesse caso valores numéricos
+            CultureInfo cultura = new CultureInfo("en-us"); 
+            Console.WriteLine(valor.ToString("C0" , cultura));
+
+            int inteiro = 256;
+            Console.WriteLine(inteiro.ToString("D10")); // colocar zeros a esquerda
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,6 +13,9 @@ namespace CursoUdemy {
                 {"Inferência - Fundamentos", Inferencia.Executar},
                 {"Interpolação - Fundamentos", Interpolacao.Executar},
                 {"Notação Ponto - Fundamentos", NotacaoPonto.Executar},
+                {"Lendo Dados - Fundamentos", LendoDados.Executar},
+                {"Formatando Número - Fundamentos", FormatandoNumero.Executar},
+                {"Conversões - Fundamentos", Conversoes.Executar},
             
             });
 
